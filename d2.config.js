635f1e6c074e9faffd4e236e90e190bcc9ee2d2b
Kripa <shresthakritisha1@gmail.com>
2024-08-02 16:47:02 +0545

    latest ecard

---
+++ d2.config.js
@@ -0,0 +1,11 @@
+const config = {
+    type: 'app',
+    name: "eCard",
+    title: "eCard",
+    description: "eCard App",
+    entryPoints: {
+        app: './src/App.js',
+    },
+}
+
+module.exports = config
